5295b625b1297257efca8e8423843c0a211d16b4
league <league@wts9.attlocal.net>
2018-04-14 12:23:28 -0700

    Date Apr. 14: Built robot drawing file

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,15 +1,24 @@
 package section2;
 
-import java.awt.Color;
+import java.awt.Color; 
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
+		Robot tbot = new Robot();
+		tbot.setSpeed(900);
 		
+		tbot.penDown();
+		
+	
+		while(true){
+		   tbot.setPenColor(Color.RED);
+		   tbot.move(20);
+		   tbot.turn(10);
+		   tbot.turn(14);
+		}
 		
 	}
 }
